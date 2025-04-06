edbb33d61635bb9d6f4acd8ea61d862151c4c8af
Christos Tricopoulos <chtr9273@rackham1.uppmax.uu.se>
2025-04-06 19:45:36 +0200

    fixed folder structure after runing non debbuged file

--- code/1_quality_controll_raw.sh
+++ code/1_quality_controll_raw.sh
@@ -7,24 +7,24 @@
 #SBATCH -J Quality_controll_raw
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user christos.tricopoulos.9273@sudent.uu.se
-#SBATCH --output=~/Genome_Analysis_Lab/code/0_output%x.%j.out
+#SBATCH --output= code/0_output%x.%j.out
 
 module load bioinfo-tools
 module load FastQC
 
-for file in ~/Genome_Analysis_Lab/DATA/raw_data/DNA_short_reads/*.fastq.gz; do
+for file in DATA/raw_data/DNA_short_reads/*.fastq.gz; do
     filename=$(basename "$file" .fastq.gz)
     folder="output_${filename}"
-    output_dir=~/Genome_Analysis_Lab/Analyses/1_preprocessing/fastQC_DNA_raw/"$folder"
+    output_dir=Analyses/1_preprocessing/fastQC_DNA_raw/"$folder"
     
     mkdir -p "$output_dir"
     fastqc -o "$output_dir" "$file"
 done
 
-for file in ~/Genome_Analysis_Lab/DATA/raw_data/RNA_short_reads/*.fastq.gz; do
+for file in DATA/raw_data/RNA_short_reads/*.fastq.gz; do
     filename=$(basename "$file" .fastq.gz)
     folder="output_${filename}"
-    output_dir=~/Genome_Analysis_Lab/Analyses/1_preprocessing/fastQC_RNA_raw/"$folder"
+    output_dir= Analyses/1_preprocessing/fastQC_RNA_raw/"$folder"
     
     mkdir -p "$output_dir"
     fastqc -o "$output_dir" "$file"
