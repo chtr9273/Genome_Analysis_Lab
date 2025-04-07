2b99c224553add43c0377954e6b0df376133bdf5
Christos Tricopoulos <chtr9273@rackham1.uppmax.uu.se>
2025-04-07 16:31:21 +0200

    Added code for post trimming quality controll

---
+++ code/3_quality_control_trim.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2025-3-3
+#SBATCH --reservation=uppmax2025-3-3_2
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 01:00:00
+#SBATCH -J Quality_control_trim
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user christos.tricopoulos.9273@student.uu.se
+#SBATCH --output=output%x.%j.out
+
+module load bioinfo-tools
+module load FastQC
+
+for file in Analyses/1_preprocessing/DNA_trimmed_data/*_paired.fq.gz; do
+    filename=$(basename "$file" .fq.gz)
+    folder="output_${filename}"
+    output_dir=Analyses/1_preprocessing/fastQC_DNA_trim/"$folder"
+
+    mkdir -p "$output_dir"
+    fastqc -o "$output_dir" "$file"
+done
+
+for file in Analyses/1_preprocessing/RNA_trimmed_data/*/*_paired.fq.gz; do
+    filename=$(basename "$file" .fq.gz)
+    folder="output_${filename}"
+    output_dir=Analyses/1_preprocessing/fastQC_RNA_trim/"$folder"
+
+    mkdir -p "$output_dir"
+    fastqc -o "$output_dir" "$file"
+done
+
+
+
