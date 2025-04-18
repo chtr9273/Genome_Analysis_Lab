a94bbd27159f76328f92505487df148646f9089e
Christos Tricopoulos <chtr9273@rackham3.uppmax.uu.se>
2025-04-18 12:56:53 +0200

    added code for only trimming DNA

---
+++ code/2_trimming/2.1_trimming_DNA.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2025-3-3
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 0:30:00
+#SBATCH -J Trimming_DNA
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user christos.tricopoulos.9273@student.uu.se
+#SBATCH --output=output%x.%j.out
+
+module load bioinfo-tools
+module load trimmomatic
+
+output_dir=Analyses/1_preprocessing/DNA_trimmed_data
+mkdir -p "$output_dir" 
+
+trimmomatic PE \
+    DATA/raw_data/DNA_short_reads/SRR24413065_1.fastq.gz DATA/raw_data/DNA_short_reads/SRR24413065_2.fastq.gz \
+    "$output_dir"/out_DNA1_paired.fq.gz "$output_dir"/out_DNA1_unpaired.fq.gz \
+    "$output_dir"/out_DNA2_paired.fq.gz "$output_dir"/out_DNA2_unpaired.fq.gz \
+    ILLUMINACLIP:$TRIMMOMATIC_ROOT/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:50
+
